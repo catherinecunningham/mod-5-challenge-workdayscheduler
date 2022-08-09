305d5499766824646c3ccd0ce179f37bb2e2d196
catherinecunningham <cfunk512@gmail.com>
2022-08-08 20:15:43 -0700

    add timeblocks

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// when calendar is opened, current date is displayed at top
+
+// time blocks with standard business hours when scroll down
+
+// time blocks are color coded to show past, present, or future
+
+// when time block is clicked, can schedule an event
+
+// click save button for time block, text for that event is saved in local storage
+
+// refresh the page, saved events persist
